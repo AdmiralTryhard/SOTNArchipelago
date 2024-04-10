b0b551a21732f036334b30d414ea8d1df101c413
nikolai <nikolai.solo.b@gmail.com>
2024-04-10 13:02:49 -0500

    added documentation and bosses list to lua

--- data/lua/connector_sotn.lua
+++ data/lua/connector_sotn.lua
@@ -16,6 +16,32 @@ local curstate =  STATE_UNINITIALIZED
 local sotnSocket = nil
 local frame = 0
 
+
+local bosses = {
+
+    [0x03CA74] = "Death Wing\'s Lair: Akmodan",
+    [0x03CA48] = "Necromancy Laboratory: Beelzebub",
+    [0x03CA5C] = "Abandoned Mine: Cerberos",
+    [0x03CA78] = "Reverse Clock Tower: Darkwing Bat",
+    [0x03CA58] = "Cave: Death",
+    [0x03CA30] = "Outer Wall: Doppleganger10",
+    [0x03CA70] = "Reverse Caverns: Doppleganger40",
+    [0x03CA54] = "Reverse Colosseum: Trevor",
+    [0x03CA7C] = "Floating Catacombs: Galamoth",
+    [0x03CA34] = "Catacombs: Granfaloon",
+    [0x03CA44] = "Royal Chapel: Hippogryph",
+    [0x03CA50] = "Clock Tower: Karasuman",
+    [0x03CA6C] = "Long Library: Lesser Demon",
+    [0x03CA64] = "Anti Chapel: Medusa",
+    [0x03CA38] = "Colosseum: Minotaur",
+    [0x03CA2C] = "Olrox\'s Quarters: Olrox",
+    [0x03CA3C] = "Underground Caverns: Scylla",
+    [0x03CA40] = "Alchemy Laboratory: Slogra",
+    --[0x03CA4C] = ""
+     --{ "Succubus", 0x03CA4C }, add this to locations.py
+     [0x03CA68] = "Reverse Outer Wall: The Creature"
+}
+
 local function defineMemoryFunctions()
 	local memDomain = {}
 	local domains = memory.getmemorydomainlist()
@@ -29,10 +55,11 @@ end
 
 local memDomain = defineMemoryFunctions()
 
-local function check_address()
+local function check_address(address)
+    -- 0x097BA0 is your current health
     memDomain.saveram()
     -- This will just check a dummy address for now. Health value works since it's at 69.
-    local health = memory.readbyte(0x097BA0)
+    local health = memory.readbyte(address)
 
     return health
 
@@ -43,6 +70,9 @@ local function give_2nd_jump()
     memory.writebyte(0x97971, 3)
 end
 
+function send()
+
+    end
 
 function receive()
     l, e = sotnSocket:receive()
@@ -92,6 +122,7 @@ function main()
         frame = frame + 1
         if (curstate == STATE_OK) or (curstate == STATE_INITIAL_CONNECTION_MADE) or (curstate == STATE_TENTATIVELY_CONNECTED) then
             receive()
+            send()
             emu.frameadvance()
         elseif (curstate == STATE_UNINITIALIZED) then
             if (frame % 60 == 0) then
@@ -105,10 +136,6 @@ function main()
                         sotnSocket:settimeout(2)
                     end
             end
-        end
-        local health = check_address()
-        if health > 70 then
-            give_2nd_jump()
         end
         emu.frameadvance()
     end
---
+++ worlds/sotn/docs/setup_en.md
@@ -0,0 +1,31 @@
+# Setup
+Nikolai
+
+
+## ROM
+
+As you know, Castlevania: Symphony of the Night (SOTN) is not part of the public domain. You MUST acquire a legal copy 
+of the ROM on your own. Place your vanilla ROM within the vanilla folder of worlds/sotn. you can then generate.
+
+
+## Generation
+
+You will need a valid YAML file to generate any Archipelago world. SOTN is pretty straight forward so far, so the
+template will be within this documentation folder. Your file outputs will be put towards a new folder in the world dir
+called ap_player_rom. Launch your cue file there to ensure that your SOTN game will recognize you as the player
+
+
+## Emulation
+
+All of this was developed for Bizhawk 2.9.1 and you will additionally need playstation core nymashock to play. Bizhawk
+does not come with the said firmware, so you will have to find it. Octoshock might work, but it is untested.
+
+
+## Connecting
+
+Load your ap_generated rom (that will be named after your player name based on your yaml), create a save file. 
+Once you are able to play as Richter in the prologue, open the SOTN client and 
+then run connector_sotn.lua found in data/lua of your base AP folder.
+
+
+# Enjoy
